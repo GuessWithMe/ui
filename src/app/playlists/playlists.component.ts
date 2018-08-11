d3e609064bbef79c6f3bf951958eb9a59119997b
Eduards Egle <eduards@egle.me>
2018-08-11 13:44:43 +0300

    Playlists component wireframe.

---
+++ src/app/playlists/playlists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  templateUrl: './playlists.component.pug',
+  styleUrls: ['./playlists.component.scss']
+})
+export class PlaylistsComponent implements OnInit {
+  constructor() {}
+
+  async ngOnInit() {
+
+  }
+}
+
